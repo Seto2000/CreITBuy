533f2d98ba8c3335911ae9bd6b57c6856c222ab1
Seto2000 <svetoslavhalev@gmail.com>
2022-03-28 01:21:26 +0300

    email sender key hide

--- CreITBuy.Core/Services/AuthMessageSenderOptions.cs
+++ CreITBuy.Core/Services/AuthMessageSenderOptions.cs
@@ -8,6 +8,6 @@ namespace CreITBuy.Core.Services
 {
     public class AuthMessageSenderOptions
     {
-        public string? SendGridKey { get; set; } = "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";
+        public string? SendGridKey { get; set; } = " ";
     }
 }
